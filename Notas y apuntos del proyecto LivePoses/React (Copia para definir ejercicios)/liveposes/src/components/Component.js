aecfc9eacca9720f34bd7e3c35d67fbe77792655
Sergio Salinero Santamaría <124250163+SergioSalinero@users.noreply.github.com>
2024-04-18 10:10:22 +0200

    Add files via upload

---
+++ Notas y apuntos del proyecto LivePoses/React (Copia para definir ejercicios)/liveposes/src/components/Component.js
@@ -0,0 +1,11 @@
+
+
+export default function Component() {
+    
+    return (
+        <div>
+            <h1>BlazePose Demo</h1>
+            
+        </div>
+    );
+}
---
+++ Notas y apuntos del proyecto LivePoses/React (Copia para definir ejercicios)/liveposes/src/pages/PoseRecognition.js
@@ -0,0 +1,10 @@
+import BlazePose from '../components/BlazePose'
+
+export default function PoseRecognition() {
+
+    return (
+        <div>
+            <BlazePose />
+        </div>
+    )
+}
