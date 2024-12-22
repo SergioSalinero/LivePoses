75dafd6520f0d1352381ffa82ba5e8903f5ad0fd
Sergio Salinero Santamaría <124250163+SergioSalinero@users.noreply.github.com>
2024-12-22 12:55:33 +0100

    Delete liveposes directory

--- liveposes/src/components/Config.js
+++
@@ -1,9 +0,0 @@
-export const POST_LOGING_URL = 'http://localhost:5000/api/auth/login'
-
-export const GET_EXERCISES_URL = 'http://localhost:5000/api/routine/get/exercises';
-export const POST_CURRENT_ROUTINE_URL = 'http://localhost:5000/api/routine/post/current_routine';
-export const GET_CURRENT_ROUTINE_URL = 'http://localhost:5000/api/routine/get/current_routine';
-export const GET_START_SIGNAL = 'http://localhost:5000/api/routine/get/start_signal';
-export const POST_PUBLIC_ROUTINE_URL = 'http://localhost:5000/api/routine/post/public_routine';
-export const GET_CATEGORY_COUNT_URL = 'http://localhost:5000/api/routine/get/category_count';
-export const GET_CATEGORY_ROUTINE_URL = 'http://localhost:5000/api/routine/get/category_routines';
